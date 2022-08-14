b16efbe06f4daf9c00826ac07e2f942b32972767
ayanrafiqi <rafiqiayan@gmail.com>
2022-08-14 23:42:33 +0530

    learned how to remove extension from Url and created 404 page option

--- indexExpress.js
+++ indexExpress.js
@@ -1,10 +1,23 @@
+const { response } = require('express');
 const express = require('express');
 const app = express();
 const path = require('path');
 const publicPath = path.join(__dirname,'Public');
-console.log(publicPath);
 
-app.use(express.static(publicPath));
 
-app.listen(8000);
+//app.use(express.static(publicPath));
+app.get('',(_,res)=>{
+    res.sendFile(`${publicPath}/index.html` );
+});
+
+app.get('/aboutme' , (_,res)=>{
+    res.sendFile(`${publicPath}/about.html`);
+
+});
+
+app.get('*' , (_,res)=>{
+    res.sendFile(`${publicPath}/404.html`);
+});
+
+app.listen(7000);
 
